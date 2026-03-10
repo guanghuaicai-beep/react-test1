b14908f71ef2994e5a0dfe4a618863a2a274ad35
guanghuaicai-beep <guanghuaicai@gmail.com>
2026-03-10 11:34:58 +0800

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-test1/',
+  build:{
+    outDir : 'dist',
+    assetsDir : 'assets',
+    rollupOptions : {
+      output : {
+        entryFileNames : `assets/[name].js`,
+        chunkFileNames : `assets/[name].js`,
+        assetFileNames : `assets/[name].[ext]`,
+      }
+    }
+  }
 })
