b78f361be4bf6d65d793580ec8fc149fddddb362
guanghuaicai-beep <guanghuaicai@gmail.com>
2026-03-11 09:49:45 +0800

    update setting for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-test1/',
+  // /react-test1/ github use
+  base: '/',
   build:{
     outDir : 'dist',
     assetsDir : 'assets',
